08d4f30494cb65b9b0dbc6845af08e29f977cfbe
Steve Mitchell <steve@steviant.co.uk>
2016-02-16 07:46:14 +0000

    Add normalize.css to npm modules, all the clean code

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,18 @@
 var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var neat        = require('node-neat').includePaths;
+var normalize   = require('node-normalize-scss').includePaths;
 var browserSync = require('browser-sync').create();
 var prompt      = require('gulp-prompt');
 
 var paths = {
-    scss: './sass/**/*.scss'
+    scss: './scss/**/*.scss'
 };
 
 gulp.task('styles', function () {
     return gulp.src(paths.scss)
         .pipe(sass({
-            includePaths: ['styles'].concat(neat)
+            includePaths: ['styles'].concat(neat, normalize)
         }))
         .pipe(gulp.dest('./'));
 });
